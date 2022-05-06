061fd97fa92ab6ae2670978dbb6bc8df45d5d72a
Vinayak Y B <vinayak.bhustali@gmail.com>
2022-05-06 19:10:47 +0530

    Shared_ptr update

---
+++ src/Sub-Task_8_SharedPtr.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<string.h>
+#include"Shape.h"
+#include"Circle.h"
+#include"Rectangle.h"
+#include"Triangle.h"
+using namespace std;
+
+int main()
+{
+	int num;
+	cout << "Enter number of Shapes:";
+	cin >> num;
+	for (int i = 0; i < num; i++) {
+		Shape Shp;
+		cout << "Enter type of shape:  (name) \n 1.Circle \n 2.Rectangle \n 3.Triangle \n";
+		cin >> Shp.type;
+		if (Shp.type == "Circle") {
+			auto c1 = std::shared_ptr<Circle>(new Circle());
+			cout << "\nEnter Radius :";
+			cin >> c1->radius; // Accessing members method 1 //
+			cout << "\nEnter Color :";
+			cin >> c1->color;
+			cout << "\nThe area is :" << c1->getArea() << "\n"; // Accessing members method 2 //
+			cout << "\nThe perimeter is :" << c1->getPerimeter() << "\n";
+			cout << "\nThe color is :" << c1->getColor() << "\n";
+		}
+		else if (Shp.type == "Rectangle") {
+			auto r1 = std::shared_ptr<Rectangle>(new Rectangle());
+			cout << "\nEnter Breadth :";
+			cin >> r1->b;
+			cout << "\nEnter Height :";
+			cin >> r1->h;
+			cout << "\nEnter Color :";
+			cin >> r1->color;
+			cout << "\nThe area is :" << r1->getArea() << "\n";
+			cout << "\nThe perimeter is :" << r1->getPerimeter() << "\n";
+			cout << "\nThe color is :" << r1->getColor() << "\n";
+		}
+		else if (Shp.type == "Triangle") {
+			auto t1 = std::shared_ptr<Triangle>(new Triangle());
+			cout << "\nEnter Base :";
+			cin >> t1->b;
+			cout << "\nEnter Height :";
+			cin >> t1->h;
+			cout << "\nEnter Hypotenuse :";
+			cin >> t1->hy;
+			cout << "\nEnter Color :";
+			cin >> t1->color;
+			cout << "\nThe area is :" << t1->getArea() << "\n";
+			cout << "\nThe perimeter is :" << t1->getPerimeter() << "\n";
+			cout << "\nThe color is :" << t1->getColor() << "\n";
+		}
+	}
+	
+}
